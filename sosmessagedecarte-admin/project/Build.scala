a6daa68f797ca591269c4e0a259e71a45f5379f9
Thomas Roger <throger@gmail.com>
2011-12-30 19:36:43 +0100

    Rename sosmessage-admin -> sosmessagedecarte-admin, sosmessage-api -> sosmessagedecarte-api

--- sosmessagedecarte-admin/project/Build.scala
+++ sosmessagedecarte-admin/project/Build.scala
@@ -4,11 +4,10 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "sosmessage-admin"
+    val appName         = "sosmessagedecarte-admin"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      //"com.mongodb.casbah" %% "casbah" % "2.1.5-1",
       "com.mongodb.casbah" %% "casbah" % "3.0.0-SNAPSHOT",
       "net.liftweb" %% "lift-json" % "2.4-M4",
       "commons-lang" % "commons-lang" % "2.6"
