4c92eec55d296520863a67b3ee66398b8ee98540
Thomas Roger <throger@gmail.com>
2011-12-30 14:56:52 +0100

    Set version to 1.0-SNAPSHOT

--- sosmessage-admin/project/Build.scala
+++ sosmessage-admin/project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "sosmessage-admin"
-    val appVersion      = "1.0"
+    val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
       //"com.mongodb.casbah" %% "casbah" % "2.1.5-1",
