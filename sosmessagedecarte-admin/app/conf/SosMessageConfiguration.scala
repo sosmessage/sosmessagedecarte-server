0d4d705ef094ccc70cc2abd03674d76fc9054893
Thomas Roger <throger@gmail.com>
2012-01-02 20:47:26 +0100

    Format

--- sosmessagedecarte-admin/app/conf/SosMessageConfiguration.scala
+++ sosmessagedecarte-admin/app/conf/SosMessageConfiguration.scala
@@ -10,19 +10,19 @@ import org.streum.configrity.Configuration
 object SosMessageConfiguration {
 
   def getConfig: Configuration = {
-      val defaultConfig = Configuration("database.host" -> "127.0.0.1",
-        "database.port" -> 27017, "database.name" -> "sosmessage")
+    val defaultConfig = Configuration("database.host" -> "127.0.0.1",
+      "database.port" -> 27017, "database.name" -> "sosmessage")
 
-      val systemConfig = Configuration.systemProperties
-      systemConfig.get[String]("sosmessage.configurationFile") match {
-        case None => defaultConfig
-        case Some(filename) =>
-          try {
-            Configuration.load(filename)
-          } catch {
-            case e: Exception => defaultConfig
-          }
-      }
+    val systemConfig = Configuration.systemProperties
+    systemConfig.get[String]("sosmessage.configurationFile") match {
+      case None => defaultConfig
+      case Some(filename) =>
+        try {
+          Configuration.load(filename)
+        } catch {
+          case e: Exception => defaultConfig
+        }
     }
+  }
 
 }
