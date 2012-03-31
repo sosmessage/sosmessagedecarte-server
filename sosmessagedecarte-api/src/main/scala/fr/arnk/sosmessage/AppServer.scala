00f87bd964f08d8e48eb180647de5630c8e0ab90
Thomas Roger <throger@gmail.com>
2012-03-31 20:46:45 +0200

    New SosMessageApiV1 object for the old API v1

--- sosmessagedecarte-api/src/main/scala/fr/arnk/sosmessage/AppServer.scala
+++ sosmessagedecarte-api/src/main/scala/fr/arnk/sosmessage/AppServer.scala
@@ -8,12 +8,12 @@ object AppServer {
 
     netty.Http(SosMessageConfig.get[Int]("server.port").getOrElse(3000))
       .handler(netty.cycle.Planify {
-        SosMessageApi.publishedCategories orElse
-          SosMessageApi.messages orElse SosMessageApi.randomMessage orElse
-          SosMessageApi.bestMessages orElse SosMessageApi.worstMessages orElse
-          SosMessageApi.postMessage orElse SosMessageApi.rateMessage orElse SosMessageApi.voteMessage orElse
-          SosMessageApi.commentsForMessage orElse SosMessageApi.postComment orElse
-          SosMessageApi.publishedAnnouncements
+        SosMessageApiV1.publishedCategories orElse
+          SosMessageApiV1.messages orElse SosMessageApiV1.randomMessage orElse
+          SosMessageApiV1.bestMessages orElse SosMessageApiV1.worstMessages orElse
+          SosMessageApiV1.postMessage orElse SosMessageApiV1.rateMessage orElse SosMessageApiV1.voteMessage orElse
+          SosMessageApiV1.commentsForMessage orElse SosMessageApiV1.postComment orElse
+          SosMessageApiV1.publishedAnnouncements
       })
       .run()
   }
